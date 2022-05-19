691de5085bbcc8be1bdda18e7cbc43f00193ab19
rahul <lbmrahulcode@gmail.com>
2022-05-19 22:08:56 +0530

    ff

---
+++ test/helpers/newAccounts.js
@@ -0,0 +1,10 @@
+
+const tronWebBuilder = require('./tronWebBuilder');
+const tronWeb = tronWebBuilder.createInstance();
+
+const amount = process.argv[2] || 10;
+
+(async function () {
+    await tronWebBuilder.newTestAccounts(amount)
+})()
+
